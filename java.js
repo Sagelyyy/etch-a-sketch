e6c5d071089925bd9511f824c3aeebc384c77ae6
Chris <orj.tsuk@gmail.com>
2021-07-13 08:24:37 -0400

    Lots of refactoring of code. Reset button now works as it should. Need to find a way to generate grid properly for the given inputs.

--- java.js
+++ java.js
@@ -7,38 +7,70 @@ const gridWidth = '830px'
 const cellHeight = '45px'
 const cellWidth = '45px'
 
+const resetButton = document.createElement('button')
+resetButton.id = 'resetGrid'
+resetButton.textContent = 'Reset (PH)'
+
+
 const flexColLeft = document.createElement('div');
 flexColLeft.style.display = "block"
 flexColLeft.style.float = 'left';
 flexColLeft.style.width = '32%';
 flexColLeft.style.height = '100%';
 
+
 const flexBoxContainer = document.createElement('div');
-flexBoxContainer.classList.add('flexContainer');
-flexBoxContainer.style.display = ('flex');
-flexBoxContainer.style.justifyContent  = ('center');
-flexBoxContainer.style.flexWrap = 'wrap';
-flexBoxContainer.style.marginLeft = '25%'
-flexBoxContainer.style.width = gridWidth;
+function createContainer() {
+    flexBoxContainer.classList.add('flexContainer');
+    flexBoxContainer.style.display = ('flex');
+    flexBoxContainer.style.justifyContent  = ('center');
+    flexBoxContainer.style.flexWrap = 'wrap';
+    flexBoxContainer.style.marginLeft = '25%'
+    flexBoxContainer.style.width = gridWidth;
+    container.appendChild(flexBoxContainer);
+}
+container.appendChild(resetButton);
 container.appendChild(flexColLeft);
-container.appendChild(flexBoxContainer);
 
 
 
+const gridDimensions = []
+let newArea = 0
+document.getElementById('resetGrid').onclick = function getGridArea(){
+    const newGridWidth = window.prompt('New Width?')
+    const newGridHeight = window.prompt('New Height?')
+    newArea = (newGridWidth * newGridHeight)
+    destroyGrid()
+    createGrid()
+    createClicks()
+}
 
 
 //here we are creating all the items(grid sqaures) we need and placing 
 //them in the flex-box container with the class of gridCell
 function createGrid(){
-    for (i = 0; i < 256; i++){
-        const flexItem = document.createElement('div');
-        flexItem.classList.add('gridCell')
-        flexItem.style.cursor = 'pointer';
-        flexItem.style.width = cellWidth;
-        flexItem.style.height = cellHeight;
-        flexItem.style.backgroundColor = bgColor
-        flexItem.style.border = '2px solid black';
-        flexBoxContainer.appendChild(flexItem);
+    if (newArea){
+        for (i = 0; i < newArea; i++){
+            const flexItem = document.createElement('div');
+            flexItem.classList.add('gridCell')
+            flexItem.style.cursor = 'pointer';
+            flexItem.style.width = cellWidth;
+            flexItem.style.height = cellHeight;
+            flexItem.style.backgroundColor = bgColor
+            flexItem.style.border = '2px solid black';
+            flexBoxContainer.appendChild(flexItem);
+        }
+    }else {
+        for (i = 0; i < 256; i++){
+            const flexItem = document.createElement('div');
+            flexItem.classList.add('gridCell')
+            flexItem.style.cursor = 'pointer';
+            flexItem.style.width = cellWidth;
+            flexItem.style.height = cellHeight;
+            flexItem.style.backgroundColor = bgColor
+            flexItem.style.border = '2px solid black';
+            flexBoxContainer.appendChild(flexItem);
+        }
     }
 }
 
@@ -49,7 +81,9 @@ function destroyGrid(){
         flexBoxContainer.removeChild(flexBoxContainer.firstChild)
     }
 }
+createContainer()
 createGrid()
+createClicks()
 
 // we have to seperate the functions for the event listeners so when we
 // later remove the listeners we dont have a scope problem
@@ -63,20 +97,23 @@ function mouseEnter(event){
     event.target.style.backgroundColor = hoverColor
 }
 
-//we are looping through all the gridCell's and adding the mouseenter
-//event to change their color!
-let gridSquares = document.getElementsByClassName('gridCell');
-for (i = 0; i < gridSquares.length; i++){
+//we are looping through all the gridCell's adding mouseover, mouseleave,
+//and click events to change the color of the cell
 
-    gridSquares[i].addEventListener("mouseenter", mouseEnter)
+function createClicks(){
+    let gridSquares = document.getElementsByClassName('gridCell');
+    for (i = 0; i < gridSquares.length; i++){
 
-    gridSquares[i].addEventListener('mouseleave', handleEvent)
+        gridSquares[i].addEventListener("mouseenter", mouseEnter)
 
-    gridSquares[i].addEventListener('click', function (fillCell) {
-        fillCell.target.style.backgroundColor = fillColor
-        fillCell.target.className = 'gridFilled'
-        fillCell.target.removeEventListener('mouseleave', handleEvent)
-        fillCell.target.removeEventListener('mouseenter', mouseEnter)
-    })
+        gridSquares[i].addEventListener('mouseleave', handleEvent)
 
+        gridSquares[i].addEventListener('click', function (fillCell) {
+            fillCell.target.style.backgroundColor = fillColor
+            fillCell.target.className = 'gridFilled'
+            fillCell.target.removeEventListener('mouseleave', handleEvent)
+            fillCell.target.removeEventListener('mouseenter', mouseEnter)
+        })
+
+    }
 }
